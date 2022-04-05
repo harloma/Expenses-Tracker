d7b58f17d52ca969e9b61087739bbbd1737f0035
harloma <topeadebanjo51@gmail.com>
2022-04-05 09:12:06 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './UI/transaction.dart';
+//import './UI/transaction_list.dart';
+import './widgets/user_transactions.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      home: Expenses(),
-    )
-  );
+void main() {
+  runApp(MaterialApp(
+    home: Expenses(),
+  ));
 }
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return ExpensesState();
   }
-
 }
 
-class ExpensesState extends State<Expenses>{
-  final List<Transaction> transaction = [
-    Transaction(id: 't1', title: 'Shoe', amount: 6000, date: DateTime.now()),
-    Transaction(id: 't1', title: 'Bag', amount: 3000, date: DateTime.now())
-  ];
+class ExpensesState extends State<Expenses> {
+  // late String titleInput;
+  // late String amountInput;
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     backgroundColor: Colors.white30,
-     appBar: AppBar(
-       backgroundColor: Colors.black,
-       title: Text('EXPENSES TRACKER'),
-       shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(
-         bottom: Radius.circular(5),
-       )),
-       leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
-       actions: [
-         IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-
-       ],
-     ),
-     body: Column(
-       children: [
-         Card(
-           child: Container(
-             margin: EdgeInsets.all(20),
-             width: double.infinity,
-             child: Text('CHART', style: TextStyle(fontSize: 50),),),
-           elevation: 10,
-         ),
-         Column(
-           children: transaction.map((tx) {
-             return Card(
-               child: Row(
-                 children: [
-                   Container(
-                     decoration: BoxDecoration(
-                       border: Border.all( color: Colors.purple, width: 6,)
-                     ),
-                     margin: EdgeInsets.all(10),
-                     padding: EdgeInsets.fromLTRB(20, 10, 10,10),
-                     //color: Colors.lightGreen,
-                     height: 80,
-                     width: 200,
-                     child: Text( '\$'+tx.amount.toString(), style: TextStyle( fontSize: 50, color: Colors.purple),),
-
-                   ),
-                   Column(
-                     children: [
-                       Text(tx.title, style: TextStyle( fontSize: 50)),
-                       Text(tx.date.toString(), style: TextStyle( fontSize: 15))
-                     ],
-
-                   )
-                 ],
-               )
-             );
-           }).toList(),
-         ),
-       ],
-     ),
-
-   );
+    return Scaffold(
+      backgroundColor: Colors.white30,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('EXPENSES TRACKER'),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(5),
+        )),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+        ],
+      ),
+      body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Card(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              width: double.infinity,
+              child: Text(
+                'CHART',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            elevation: 5,
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            //height: 160,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+          UserTransactions()
+        ],
+      ),
+    ),
+    );
   }
-
 }
